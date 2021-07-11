fa638d95797a00c3e6bed6f5457a3f4a899ccbe4
franciscocid <franciscocidadehomemjunior@gmail.com>
2021-07-11 17:04:03 -0300

    Generating properties from struct

--- parser/generator.go
+++ parser/generator.go
@@ -1,10 +1,33 @@
 package parser
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func GenerateFromMap(in map[string]string) (out string) {
 	for key, value := range in {
 		out += fmt.Sprintf("%s: %s\n", key, value)
 	}
+	return
+}
+
+func GenerateFromStruct(in interface{}) (out string) {
+	fields := reflect.TypeOf(in)
+	values := reflect.ValueOf(in)
+
+	num := fields.NumField()
+
+	for i := 0; i < num; i++ {
+		field := fields.Field(i)
+		value := values.Field(i)
+
+		tagName := field.Tag.Get("properties")
+		if len(tagName) == 0 {
+			tagName = field.Name
+		}
+		out += fmt.Sprintf("%s: %s\n", tagName, value.String())
+	}
+
 	return
 }
--- parser/generator_test.go
+++ parser/generator_test.go
@@ -1,14 +1,14 @@
 package parser
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerator(t *testing.T) {
-
-	t.Run("Should generate a colon separated text", func(t *testing.T) {
+	t.Run("Should generate a colon separated text from a map", func(t *testing.T) {
 		mp := map[string]string{
 			"website":  "https://en.wikipedia.org/",
 			"language": "English",
@@ -17,6 +17,26 @@ func TestGenerator(t *testing.T) {
 		want := `website: https://en.wikipedia.org/
 language: English
 `
+		fmt.Println(have)
+		assert.Equal(t, want, have)
+	})
+
+	t.Run("Should generate a colon separated text from a struct", func(t *testing.T) {
+		strc := struct {
+			Website   string `properties:"website"`
+			Language  string `properties:"language"`
+			language2 string
+		}{
+			Website:   "https://en.wikipedia.org/",
+			Language:  "English",
+			language2: "English",
+		}
+		have := GenerateFromStruct(strc)
+		want := `website: https://en.wikipedia.org/
+language: English
+language2: English
+`
+		fmt.Println(have)
 		assert.Equal(t, want, have)
 	})
 }
