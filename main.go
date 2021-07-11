e888e58a9acfce22336f5535a797dc409f7ae7d9
franciscocid <franciscocidadehomemjunior@gmail.com>
2021-07-11 16:26:36 -0300

    Testing example

--- main.go
+++ main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/go-dot-properties/parser"
+)
+
+func Parse(text string) (interface{}, error) {
+	tokenizer := parser.NewTokenizer(text)
+	tokens := tokenizer.Tokenize()
+	pars := parser.NewParser(tokens)
+	return pars.ParseToMap()
+}
 
 func main() {
-	// TODO: Sys args
-	fmt.Println("Test")
+
+	fmt.Println(os.Args)
+	filepath := ""
+
+	if len(os.Args) >= 2 {
+		filepath = os.Args[1]
+	}
+
+	bytes, err := os.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	text := string(bytes)
+
+	mp, err := Parse(text)
+	fmt.Printf("%#v, %e\n", mp, err)
+
+	js, err := json.Marshal(mp)
+	fmt.Println(string(js), err)
+
 }
